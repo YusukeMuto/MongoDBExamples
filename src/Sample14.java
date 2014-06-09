0cabdafad43834fecb28aa7f06f174b86708ea41
Takeshi Morita <takesi.morita@gmail.com>
2014-06-09 16:24:21 +0900

    Modified Sample14.java

--- src/Sample14.java
+++ src/Sample14.java
@@ -21,21 +21,21 @@ public class Sample14 {
 
 	public static void main(String[] args) throws InterruptedException {
 		try {
-			DBCollection c1 = getBookCollection("localhost", 40000);
-			c1.slaveOk(); // 非推奨
-			DBCollection c2 = getBookCollection("localhost", 40001);
-			System.out.println("Before S40000: ");
-			showCollection(c1);
-			System.out.println("Before P40001: ");
-			showCollection(c2);
-			System.out.println("Insert title: こころ into P40001");
+			DBCollection pc = getBookCollection("localhost", 40000);
+			DBCollection sc = getBookCollection("localhost", 40001);
+			sc.slaveOk(); // 非推奨 (Secondary Nodeに対して実行）
+			System.out.println("Before Primary Node (40000): ");
+			showCollection(pc);
+			System.out.println("Before Secondary Node (40001): ");
+			showCollection(sc);
+			System.out.println("Insert title: こころ into Primary Node (40000)");
 			BasicDBObject doc = new BasicDBObject("title", "こころ");
-			c2.insert(doc);
-			Thread.sleep(5000);
-			System.out.println("After S40000: ");
-			showCollection(c1);
-			System.out.println("After P40001: ");
-			showCollection(c2);
+			pc.insert(doc);
+			// Thread.sleep(5000);
+			System.out.println("After Primary Node (40000): ");
+			showCollection(pc);
+			System.out.println("After Secondary Node (40001): ");
+			showCollection(sc);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
